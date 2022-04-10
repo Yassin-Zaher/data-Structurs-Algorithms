4a9a1ac6407229508b6990a211bd976a1d066b56
Yassin Zaher <yassinzaherelfeliri@gmail.com>
2022-04-10 20:10:53 +0000

    Converting singly linked list to doubly linked list

--- linkedList.js
+++ linkedList.js
@@ -1,25 +1,55 @@
-class LinkedList{
-    constructor(value){
-        this.head = {value : value, next : null}
-        this.tail = head
-        this.length = 1
+// TODO : Convert this to a doubly linked list
+
+class DoublyLinkedList {
+  constructor(value) {
+    this.head = { value: value, next: null, prev: null };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = { value: value, next: null, prev: null };
+    length += 1;
+    this.tail.next = newNode;
+    newNode.prev = this.tail;
+    this.tail = newNode;
+    return this;
+  }
+  prepend(value) {
+    // adding a value to the start of our linked List
+    const newNode = {
+      value: value,
+      next: null,
+      prev: null,
+    };
+    newNode.next = this.head;
+    this.head.prev = newNode;
+    this.head = newNode;
+    this.length++;
+  }
+
+  insert(index, value) {
+    const newNode = { value: value, next: null, prev: null };
+    //check params
+    if (index > this.length) {
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+      return;
     }
-    append(value){
-        const newNode = {value: value, next: null}
-        length += 1
-        this.tail.next = newNode
-        this.tail = newNode
-        return this;
- 
+
+    let count = 0;
+    let currentNode = this.head;
+    let nextNodeHolder = currentNode.next;
+    while (currentNode.next) {
+      if (index - 1 === count) {
+        currentNode.next = newNode;
+        newNode.next = nextNodeHolder;
+        newNode.prev = currentNode;
+        nextNodeHolder.prev = newNode;
+        break;
+      }
+      currentNode = currentNode.next;
+      count++;
     }
-    prepend(value){
-        //adding a value to the start of our linked List
-        const newNode = {
-            value: value,
-            next: null
-        };
-        newNode.next = this.head
-        this.head = newNode
-        this.length++
-    }
+  }
 }
